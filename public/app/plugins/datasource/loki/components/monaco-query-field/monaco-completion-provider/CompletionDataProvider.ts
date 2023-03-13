426e59579147e65eb2d1479f2027f76053c94a32
Bogdan Matei <bogdan@matei.dev>
2023-03-13 22:33:10 +0200

    Fix LS

--- public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/CompletionDataProvider.ts
+++ public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/CompletionDataProvider.ts
@@ -28,11 +28,229 @@ export class CompletionDataProvider {
     this.webSocket.addEventListener('open', () => {
       this.webSocket.send(
         JSON.stringify({
+          id: this.webSocketId++,
+          jsonrpc: '2.0',
+          method: 'initialize',
+          params: {
+            processId: null,
+            clientInfo: { name: 'Monaco', version: '1.69.0' },
+            locale: 'en-GB',
+            rootPath: null,
+            rootUri: null,
+            capabilities: {
+              workspace: {
+                applyEdit: true,
+                workspaceEdit: {
+                  documentChanges: true,
+                  resourceOperations: ['create', 'rename', 'delete'],
+                  failureHandling: 'textOnlyTransactional',
+                  normalizesLineEndings: true,
+                  changeAnnotationSupport: { groupsOnLabel: true },
+                },
+                codeLens: { refreshSupport: true },
+                executeCommand: { dynamicRegistration: true },
+                workspaceFolders: true,
+                semanticTokens: { refreshSupport: true },
+                inlayHint: { refreshSupport: true },
+                diagnostics: { refreshSupport: true },
+              },
+              textDocument: {
+                publishDiagnostics: {
+                  relatedInformation: true,
+                  versionSupport: false,
+                  tagSupport: { valueSet: [1, 2] },
+                  codeDescriptionSupport: true,
+                  dataSupport: true,
+                },
+                synchronization: { dynamicRegistration: true },
+                completion: {
+                  dynamicRegistration: true,
+                  contextSupport: true,
+                  completionItem: {
+                    snippetSupport: true,
+                    commitCharactersSupport: true,
+                    documentationFormat: ['markdown', 'plaintext'],
+                    deprecatedSupport: true,
+                    preselectSupport: true,
+                    tagSupport: { valueSet: [1] },
+                    insertReplaceSupport: true,
+                    resolveSupport: { properties: ['documentation', 'detail', 'additionalTextEdits'] },
+                    insertTextModeSupport: { valueSet: [1, 2] },
+                    labelDetailsSupport: true,
+                  },
+                  insertTextMode: 2,
+                  completionItemKind: {
+                    valueSet: [
+                      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                    ],
+                  },
+                  completionList: {
+                    itemDefaults: ['commitCharacters', 'editRange', 'insertTextFormat', 'insertTextMode'],
+                  },
+                },
+                hover: { dynamicRegistration: true, contentFormat: ['markdown', 'plaintext'] },
+                signatureHelp: {
+                  dynamicRegistration: true,
+                  signatureInformation: {
+                    documentationFormat: ['markdown', 'plaintext'],
+                    parameterInformation: { labelOffsetSupport: true },
+                    activeParameterSupport: true,
+                  },
+                  contextSupport: true,
+                },
+                definition: { dynamicRegistration: true, linkSupport: true },
+                references: { dynamicRegistration: true },
+                documentHighlight: { dynamicRegistration: true },
+                documentSymbol: {
+                  dynamicRegistration: true,
+                  symbolKind: {
+                    valueSet: [
+                      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                    ],
+                  },
+                  hierarchicalDocumentSymbolSupport: true,
+                  tagSupport: { valueSet: [1] },
+                  labelSupport: true,
+                },
+                codeAction: {
+                  dynamicRegistration: true,
+                  isPreferredSupport: true,
+                  disabledSupport: true,
+                  dataSupport: true,
+                  resolveSupport: { properties: ['edit'] },
+                  codeActionLiteralSupport: {
+                    codeActionKind: {
+                      valueSet: [
+                        '',
+                        'quickfix',
+                        'refactor',
+                        'refactor.extract',
+                        'refactor.inline',
+                        'refactor.rewrite',
+                        'source',
+                        'source.organizeImports',
+                      ],
+                    },
+                  },
+                  honorsChangeAnnotations: false,
+                },
+                codeLens: { dynamicRegistration: true },
+                formatting: { dynamicRegistration: true },
+                rangeFormatting: { dynamicRegistration: true },
+                onTypeFormatting: { dynamicRegistration: true },
+                rename: {
+                  dynamicRegistration: true,
+                  prepareSupport: true,
+                  prepareSupportDefaultBehavior: 1,
+                  honorsChangeAnnotations: true,
+                },
+                documentLink: { dynamicRegistration: true, tooltipSupport: true },
+                typeDefinition: { dynamicRegistration: true, linkSupport: true },
+                implementation: { dynamicRegistration: true, linkSupport: true },
+                colorProvider: { dynamicRegistration: true },
+                foldingRange: {
+                  dynamicRegistration: true,
+                  rangeLimit: 5000,
+                  lineFoldingOnly: true,
+                  foldingRangeKind: { valueSet: ['comment', 'imports', 'region'] },
+                  foldingRange: { collapsedText: false },
+                },
+                declaration: { dynamicRegistration: true, linkSupport: true },
+                selectionRange: { dynamicRegistration: true },
+                semanticTokens: {
+                  dynamicRegistration: true,
+                  tokenTypes: [
+                    'namespace',
+                    'type',
+                    'class',
+                    'enum',
+                    'interface',
+                    'struct',
+                    'typeParameter',
+                    'parameter',
+                    'variable',
+                    'property',
+                    'enumMember',
+                    'event',
+                    'function',
+                    'method',
+                    'macro',
+                    'keyword',
+                    'modifier',
+                    'comment',
+                    'string',
+                    'number',
+                    'regexp',
+                    'operator',
+                    'decorator',
+                  ],
+                  tokenModifiers: [
+                    'declaration',
+                    'definition',
+                    'readonly',
+                    'static',
+                    'deprecated',
+                    'abstract',
+                    'async',
+                    'modification',
+                    'documentation',
+                    'defaultLibrary',
+                  ],
+                  formats: ['relative'],
+                  requests: { range: true, full: { delta: true } },
+                  multilineTokenSupport: false,
+                  overlappingTokenSupport: false,
+                  serverCancelSupport: true,
+                  augmentsSyntaxTokens: true,
+                },
+                linkedEditingRange: { dynamicRegistration: true },
+                inlayHint: {
+                  dynamicRegistration: true,
+                  resolveSupport: {
+                    properties: ['tooltip', 'textEdits', 'label.tooltip', 'label.location', 'label.command'],
+                  },
+                },
+                diagnostic: { dynamicRegistration: true, relatedDocumentSupport: false },
+              },
+              window: {
+                showMessage: { messageActionItem: { additionalPropertiesSupport: true } },
+                showDocument: { support: true },
+              },
+              general: {
+                staleRequestSupport: {
+                  cancel: true,
+                  retryOnContentModified: [
+                    'textDocument/semanticTokens/full',
+                    'textDocument/semanticTokens/range',
+                    'textDocument/semanticTokens/full/delta',
+                  ],
+                },
+                regularExpressions: { engine: 'ECMAScript', version: 'ES2020' },
+                markdown: { parser: 'marked', version: '1.1.0' },
+                positionEncodings: ['utf-16'],
+              },
+            },
+            trace: 'off',
+            workspaceFolders: null,
+          },
+        })
+      );
+
+      this.webSocket.send(
+        JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'initialized',
+          params: {},
+        })
+      );
+
+      this.webSocket.send(
+        JSON.stringify({
           jsonrpc: '2.0',
           method: 'textDocument/didOpen',
           params: {
             textDocument: {
-              languageId: 'json',
+              languageId: 'logql',
               text: initialValue,
               uri: 'inmemory://model.json',
               version: 1,
@@ -61,7 +279,7 @@ export class CompletionDataProvider {
 
   async getLSCompletions(position: monacoTypes.Position): Promise<monacoTypes.languages.CompletionItem[]> {
     return new Promise((success, error) => {
-      if (!this.webSocket) {
+      if (!this.webSocket || this.webSocket.readyState !== this.webSocket.OPEN) {
         return success([]);
       }
 
@@ -81,25 +299,23 @@ export class CompletionDataProvider {
         } catch (err) {}
       });
 
-      if (webSocket.OPEN && !webSocket.CONNECTING && !webSocket.CLOSING) {
-        webSocket.send(
-          JSON.stringify({
-            method: 'textDocument/completion',
-            params: {
-              textDocument: {
-                uri: 'inmemory://model.json',
-              },
-              position: {
-                character: position.column - 1,
-                line: position.lineNumber - 1,
-              },
-              context: { triggerKind: 1 },
+      webSocket.send(
+        JSON.stringify({
+          method: 'textDocument/completion',
+          params: {
+            textDocument: {
+              uri: 'inmemory://model.json',
             },
-            jsonrpc: '2.0',
-            id: id,
-          })
-        );
-      }
+            position: {
+              character: position.column - 1,
+              line: position.lineNumber - 1,
+            },
+            context: { triggerKind: 1 },
+          },
+          jsonrpc: '2.0',
+          id: id,
+        })
+      );
     });
   }
 
