1dcc930320d3da4d7178afb81935df0f5be42d17
Matias Chomicki <matyax@gmail.com>
2023-03-14 14:26:04 +0100

    Apply prompt engineering to get logql

--- public/app/plugins/datasource/loki/querybuilder/ai.ts
+++ public/app/plugins/datasource/loki/querybuilder/ai.ts
@@ -1,23 +1,77 @@
-const { Configuration, OpenAIApi } = require('openai');
+import { Configuration, OpenAIApi } from 'openai';
+
+import { explainOperator, getOperationDefinitions } from './operations';
+
 const configuration = new Configuration({});
 const openai = new OpenAIApi(configuration);
 
-const prependedText = 'You will only answer with Grafan Loki queries.';
+const prependedText = `// Sample LogQL loki queries:
+// Select log lines from the stream test and value 123
+{test="123"}
+// Select log lines of test 123
+{test="123"}
+// Write query for test = 123
+{test="123"}
+// Select logs of test equals 123
+{test="123"}
+// Show logs of test 123
+{test="123"}
+// When test is not 123
+{test!="123"}
+// When test is different than 123
+{test!="123"}
+// Select test 123 logs and filter lines that contain text
+{test="123"} |= "text"
+// Show test 123 lines that contain text
+{test="123"} |= "text"
+// Filter test different 123 not containing text
+{test!="123"} != text
+// Select stream test containing 123
+{test~="123"}
+// Select stream with regex mysql.+
+{test~="mysql.+"}
+// Show logs not matching test mysql.+ regexp
+{test!~"mysql.+"}
+`;
 
 export async function ask(prompt: string) {
   let response;
   try {
     response = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: `${prependedText} ${prompt}`,
+      prompt: `${prependedText} // Write a logql query to ${prompt}`,
       temperature: 0.2,
-      max_tokens: 30,
+      max_tokens: 60,
     });
 
     return response.data?.choices[0]?.text || '';
   } catch (e) {
     return 'We could not process your request.';
   }
+}
 
-  return response;
+type TrainingType = {
+  prompt: string;
+  completion: string;
+};
+
+export function getTrainingData() {
+  const operations = getOperationDefinitions();
+
+  const operationTraining: TrainingType[] = operations.map((operation) => ({
+    prompt: explainOperator(operation.id),
+    completion: operation.name,
+  }));
+
+  const sampleModel = {
+    id: '',
+    params: ['value'],
+  };
+
+  const renderTraining: TrainingType[] = operations.map((operation) => ({
+    prompt: `How do you write the Grafana Loki query for ${operation.name}`,
+    completion: operation.renderer({ ...sampleModel, id: operation.id }, operation, ''),
+  }));
+
+  return [...operationTraining, ...renderTraining];
 }
