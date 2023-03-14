c17f2a765c1ebf6f2714cdd60bf23e5afa85f6d0
drew08t <drew08@gmail.com>
2023-03-14 15:17:33 -0700

    Add wind turbine demo from gdev json

---
+++ public/app/features/scenes/scenes/canvasDemoQueries.ts
@@ -0,0 +1,48 @@
+import { QueryRunnerState, SceneQueryRunner } from '@grafana/scenes';
+import { TestData } from 'app/plugins/datasource/testdata/dataquery.gen';
+
+export enum queries {
+  Wind,
+  SolarDay,
+  SolarNight,
+}
+
+export function getCanvasDemoQuery(
+  query: queries,
+  overrides?: Partial<TestData>,
+  queryRunnerOverrides?: Partial<QueryRunnerState>
+) {
+  switch (query) {
+    case queries.Wind: {
+      return new SceneQueryRunner({
+        queries: [
+          {
+            csvContent:
+              'w1_rpm, w1_energy_output, w1_status, w2_rpm, w2_energy_output, w2_status, w3_rpm, w3_energy_output, w3_status, w4_rpm, w4_energy_output, w4_status\n12, 1.2, operational, 22, 1.8, operational, 8, 0.6, operational, 0, 0, needs maintenance',
+            datasource: {
+              type: 'testdata',
+              uid: 'PD8C576611E62080A',
+            },
+            refId: 'A',
+            scenarioId: 'csv_content',
+          },
+        ],
+        ...queryRunnerOverrides,
+      });
+    }
+  }
+  return new SceneQueryRunner({
+    queries: [
+      {
+        refId: 'A',
+        datasource: {
+          uid: 'gdev-testdata',
+          type: 'testdata',
+        },
+        scenarioId: 'random_walk',
+        ...overrides,
+      },
+    ],
+    ...queryRunnerOverrides,
+  });
+}
