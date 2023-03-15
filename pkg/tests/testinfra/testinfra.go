7125fe3554c73acb4f89e849375fc67e14044db8
Bryan Boreham <bjboreham@gmail.com>
2023-03-15 19:31:29 +0000

    testinfra: wait 10ms after starting Grafana
    This seems to avoid some race condition which the 10ms sleep in every
    DB request was previously masking.

--- pkg/tests/testinfra/testinfra.go
+++ pkg/tests/testinfra/testinfra.go
@@ -66,6 +66,7 @@ func StartGrafanaEnv(t *testing.T, grafDir, cfgPath string) (string, *server.Tes
 	})
 
 	// Wait for Grafana to be ready
+	time.Sleep(10 * time.Millisecond)
 	addr := listener.Addr().String()
 	resp, err := http.Get(fmt.Sprintf("http://%s/api/health", addr))
 	require.NoError(t, err)
