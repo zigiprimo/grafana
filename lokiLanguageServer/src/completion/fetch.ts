9d38237d2ddeeb7f9ab2437e392c946cba275fd4
Ivana Huckova <ivana.huckova@gmail.com>
2023-03-15 22:09:07 +0100

    Add API token for server requests

--- lokiLanguageServer/src/completion/fetch.ts
+++ lokiLanguageServer/src/completion/fetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // These need to be set up
 const DS_UID = 'cd82b426-8a65-4e2d-aec8-ef37f5e4f568';
-const COOKIE = '_ga=...';
+const API_TOKEN = 'abc123';
 export async function getLabelNames(): Promise<string[]> {
   const url = `http://localhost:3000/api/datasources/uid/${DS_UID}/resources/labels?a`;
 
@@ -11,7 +11,7 @@ export async function getLabelNames(): Promise<string[]> {
       'x-datasource-uid': 'cd82b426-8a65-4e2d-aec8-ef37f5e4f568',
       'x-grafana-org-id': '1',
       'x-plugin-id': 'loki',
-      cookie: COOKIE,
+      Authorization: `Bearer ${API_TOKEN}`,
     },
     method: 'GET',
   });
@@ -28,7 +28,7 @@ export async function getLabelValues(labelName: string): Promise<string[]> {
       'x-datasource-uid': 'cd82b426-8a65-4e2d-aec8-ef37f5e4f568',
       'x-grafana-org-id': '1',
       'x-plugin-id': 'loki',
-      cookie: COOKIE,
+      Authorization: `Bearer ${API_TOKEN}`,
     },
     method: 'GET',
   });
