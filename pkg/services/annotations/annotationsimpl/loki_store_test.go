5c0b87d3955e09d0cdcd481e8f28d7e3af57057e
Ying WANG <ying.wang@grafana.com>
2023-03-15 16:16:20 +0100

    make unittest start using loki store

--- pkg/services/annotations/annotationsimpl/loki_store_test.go
+++ pkg/services/annotations/annotationsimpl/loki_store_test.go
@@ -3,18 +3,33 @@ package annotationsimpl
 import (
 	"context"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
+	"github.com/grafana/grafana/pkg/infra/db"
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/services/tag/tagimpl"
+	"github.com/grafana/grafana/pkg/setting"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIntegrationLokiAnnotations(t *testing.T) {
 	// t.Skip("skipping integration test")
+	lokiHost, err := url.Parse("http://localhost:3100")
+	require.NoError(t, err)
+	lokiConfig := lokiConfig{
+		ReadPathURL:    lokiHost,
+		WritePathURL:   lokiHost,
+		TenantID:       "?",
+		ExternalLabels: make(map[string]string),
+	}
 
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -35,7 +50,15 @@ func TestIntegrationLokiAnnotations(t *testing.T) {
 		Image: "grafana/loki",
 		// Cmd:   []string{"echo", "hello world"},
 		Tty: false,
-	}, nil, nil, "")
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			nat.Port("3100/tcp"): []nat.PortBinding{
+				{
+					HostPort: "3100",
+				},
+			},
+		},
+	}, nil, "")
 	if err != nil {
 		panic(err)
 	}
@@ -51,16 +74,15 @@ func TestIntegrationLokiAnnotations(t *testing.T) {
 
 	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 
-	time.Sleep(8 * time.Second)
+	testIntegrationAnnotations(t, func(db db.DB, cfg *setting.Cfg, maximumTagsLength int64) store {
+		return &lokiRepositoryImpl{
+			db:                db,
+			cfg:               setting.NewCfg(),
+			log:               log.New("annotation.loki.test"),
+			tagService:        tagimpl.ProvideService(db, cfg),
+			maximumTagsLength: maximumTagsLength,
+			httpLokiClient:    newLokiClient(lokiConfig, &http.Client{}, log.New("annotation.loki.test"))}
+	})
 
-	cli.ContainerStop(ctx, resp.ID, nil)
-
-	// testIntegrationAnnotations(t, func(db db.DB, cfg *setting.Cfg, maximumTagsLength int64) store {
-	// 	return &lokiRepositoryImpl{
-	// 		db:                db,
-	// 		cfg:               setting.NewCfg(),
-	// 		log:               log.New("annotation.loki.test"),
-	// 		tagService:        tagimpl.ProvideService(db, cfg),
-	// 		maximumTagsLength: maximumTagsLength}
-	// })
+	defer cli.ContainerStop(ctx, resp.ID, nil)
 }
