e63de2424fb00cb405441d010aa7b3344009454c
suntala <arati.rana@grafana.com>
2023-03-15 14:13:04 +0100

    Add filtering of selectors for get (continued)

--- pkg/services/annotations/annotationsimpl/loki_store.go
+++ pkg/services/annotations/annotationsimpl/loki_store.go
@@ -1,17 +1,22 @@
 package annotationsimpl
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/log"
 	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/annotations"
+	"github.com/grafana/grafana/pkg/services/dashboards"
+	"github.com/grafana/grafana/pkg/services/sqlstore/permissions"
+	"github.com/grafana/grafana/pkg/services/sqlstore/searchstore"
 	"github.com/grafana/grafana/pkg/services/tag"
 	"github.com/grafana/grafana/pkg/services/user"
 	"github.com/grafana/grafana/pkg/setting"
@@ -92,7 +97,7 @@ func (r *lokiRepositoryImpl) Get(ctx context.Context, query *annotations.ItemQue
 	}
 
 	if !ac.IsDisabled(r.cfg) {
-		selectors, err = r.filterByAccessControl(ctx, selectors, query.SignedInUser)
+		selectors, err = r.filterByAccessControl(ctx, selectors, query)
 		if err != nil {
 			return nil, err
 		}
@@ -136,51 +141,81 @@ func (r *lokiRepositoryImpl) Get(ctx context.Context, query *annotations.ItemQue
 	return items, nil
 }
 
-func (r *lokiRepositoryImpl) filterByAccessControl(ctx context.Context, selectors []selector, user *user.SignedInUser) ([]selector, error) {
+func (r *lokiRepositoryImpl) filterByAccessControl(ctx context.Context, selectors []selector, query *annotations.ItemQuery) ([]selector, error) {
+	acFilter, acArgs, err := getAccessControlFilterAlternate(query.SignedInUser)
+	if err != nil {
+		return nil, err
+	}
+
+	dashboardIDs := make([]string, 0)
+	err = r.db.WithDbSession(ctx, func(sess *db.Session) error {
+		var sql bytes.Buffer
+		// #TODO any checks to do for OrgID before?
+		sql.WriteString(fmt.Sprintf("SELECT id FROM dashboard WHERE org_id = %d AND (%s)", query.OrgID, acFilter))
+		if err := sess.SQL(sql.String(), acArgs...).Find(&dashboardIDs); err != nil {
+			dashboardIDs = nil
+			return err
+		}
+		// #TODO: as a first step make a database call to find all dashboards for which the user has permissions
+		// use this to add the appropriate selectors
+		// next step would be to pass dashboard_uid as a label in the call to Add() and then use that when filtering
+		// EDIT about passing dashboard_uid: considering user.Permissions aren't to be relied on, we still need to
+		// query the permissions table either way so we might as well continuing querying the dashboards table. Passing the
+		// dashboard_uid label is no longer required for filtering purposes.
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// #TODO: if we want to skip the use of regex here, we could filter loki results instead of
+	// the selectors we send to loki. The advantage of filtering selectors is that we can check
+	// some permissions before ever querying loki (for example user having permissions for the org)
+	// and we don't have to do permission checks in two places.
+	// However I imagine the list of IDs we include in the regex could get quite long. That's a potential issue.
+	if len(dashboardIDs) > 0 {
+		selectors = append(selectors, selector{
+			label: "dashboard_id",
+			op:    eqRegEx,
+			value: fmt.Sprintf("(%s)", strings.Join(dashboardIDs, "|")),
+		})
+	}
+	return selectors, err
+}
+
+// #TODO: fix naming. This is an almost exact copy of the one in xorm_store.go but some parts of the query string have
+// been modified to remove references to the larger query string we aren't using.
+func getAccessControlFilterAlternate(user *user.SignedInUser) (string, []interface{}, error) {
 	if user == nil || user.Permissions[user.OrgID] == nil {
-		return nil, errors.New("missing permissions")
+		return "", nil, errors.New("missing permissions")
 	}
 	scopes, has := user.Permissions[user.OrgID][ac.ActionAnnotationsRead]
 	if !has {
-		return nil, errors.New("missing permissions")
+		return "", nil, errors.New("missing permissions")
 	}
 	types, hasWildcardScope := ac.ParseScopes(ac.ScopeAnnotationsProvider.GetResourceScopeType(""), scopes)
 	if hasWildcardScope {
 		types = map[interface{}]struct{}{annotations.Dashboard.String(): {}, annotations.Organization.String(): {}}
 	}
 
+	var filters []string
+	var params []interface{}
 	for t := range types {
 		// annotation read permission with scope annotations:type:organization allows listing annotations that are not associated with a dashboard
 		if t == annotations.Organization.String() {
-			// TODO: improve the creation of the new selector here (using labels, newSelector etc)
-			selectors = append(selectors, selector{label: "dashboard_id", op: eq, value: "0"})
+			filters = append(filters, "id = 0")
 		}
 		// annotation read permission with scope annotations:type:dashboard allows listing annotations from dashboards which the user can view
 		if t == annotations.Dashboard.String() {
-			var sql bytes.Buffer
 			dashboardFilter, dashboardParams := permissions.NewAccessControlDashboardPermissionFilter(user, dashboards.PERMISSION_VIEW, searchstore.TypeDashboard).Where()
-			sql.WriteString(fmt.Sprintf("SELECT id FROM dashboard WHERE %s", dashboardFilter))
 
-			err := r.db.WithDbSession(ctx, func(sess *db.Session) error {
-				ids := make([]string, 0)
-				if err := sess.SQL(sql.String(), dashboardParams...).Find(&ids); err != nil {
-					ids = nil
-					return err
-				}
-				// #TODO: as a first step make a database call to find all dashboards for which the user has permissions
-				// use this to add the appropriate selectors
-				// next step would be to pass dashboard_uid as a label in the call to Add() and then use that when filtering
-				// EDIT about passing dashboard_uid: considering user.Permissions aren't to be relied on, we still need to
-				// query the permissions table either way so we might as well continuing querying the dashboards table. Passing the
-				// dashboard_uid label is no longer required for filtering purposes.
-				return nil
-			})
-			if err != nil {
-				return nil, err
-			}
+			filter := fmt.Sprintf("id IN(SELECT id FROM dashboard WHERE %s)", dashboardFilter)
+
+			filters = append(filters, filter)
+			params = dashboardParams
 		}
 	}
-	return selectors, nil
+	return strings.Join(filters, " OR "), params, nil
 }
 
 func (r *lokiRepositoryImpl) Delete(ctx context.Context, params *annotations.DeleteParams) error {
