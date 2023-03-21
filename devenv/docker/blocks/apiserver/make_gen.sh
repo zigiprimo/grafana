87c3d215ad4264af5da40677b430101bd037a079
Charandas Batra <charandas.batra@grafana.com>
2023-03-21 13:22:48 -0700

    Bring cert generation up to speed for authn/authz client certs

--- devenv/docker/blocks/apiserver/make_gen.sh
+++ devenv/docker/blocks/apiserver/make_gen.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 pushd /tmp
 go install github.com/cloudflare/cfssl/cmd/...@latest
 popd
-rm -rf certs final
-cfssl genkey -initca csr.json | cfssljson -bare ca
-mkdir -p certs final
-mv ca* certs/
-cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -hostname=host.docker.internal csr.json | cfssljson -bare out
-mv out* final/
+rm -rf certs
+
+mkdir -p certs/webhooks certs/authn certs/authz
+
+cfssl genkey -initca ca.json | cfssljson -bare ca
+mv ca*.pem certs/
+
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem webhooks.json | cfssljson -bare webhooks
+mv webhooks*.pem certs/webhooks/
+
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -profile=client authn-webhook-user.json | cfssljson -bare authn-webhook-user
+mv authn-webhook-user*.pem authn/
+
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -profile=client authz-webhook-user.json | cfssljson -bare authz-webhook-user
+mv authz-webhook-user*.pem authz/
+
+rm -rf *.csr
 
 echo 'PUT THIS IN VALIDATING WEBHOOK CONFIGURATION'
 echo 'clientConfig'
 echo '  caBundle: {BASE64}'
-echo 'writing base64 encoded cert to final/base_64_cert.pem'
-cat certs/ca.pem | base64 >> final/base_64_cert.pem
+echo 'writing base64 encoded cert to webhooks/base_64_cert.pem'
+cat certs/ca.pem | base64 >> certs/webhooks/base_64_cert.pem
