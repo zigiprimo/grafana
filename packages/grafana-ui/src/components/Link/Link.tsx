8b55d376b6ccddd0815ef72648e2871e3ed957b4
Torkel Ödegaard <torkel@grafana.com>
2023-04-28 09:59:00 +0200

    Link color

--- packages/grafana-ui/src/components/Link/Link.tsx
+++ packages/grafana-ui/src/components/Link/Link.tsx
@@ -1,14 +1,22 @@
 import React, { AnchorHTMLAttributes, forwardRef } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { locationUtil, textUtil } from '@grafana/data';
+import { GrafanaTheme2, locationUtil, textUtil } from '@grafana/data';
 
-export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  /** Override the default weight for the used variant */
+  weight?: 'regular' | 'medium';
+
+  /**
+   * Defaults to link color
+   */
+  color?: keyof GrafanaTheme2['colors']['text'];
+}
 
 /**
  * @alpha
  */
-export const Link = forwardRef<HTMLAnchorElement, Props>(({ href, children, ...rest }, ref) => {
+export const Link = forwardRef<HTMLAnchorElement, Props>(({ href, color, weight, children, ...rest }, ref) => {
   const validUrl = locationUtil.stripBaseFromUrl(textUtil.sanitizeUrl(href ?? ''));
 
   return (
