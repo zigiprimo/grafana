0d7e7b84a71300773c1c2835a45d6b6591f9def9
suntala <arati.rana@grafana.com>
2023-05-11 23:13:41 +0200

    Add basic sqlite implementation

---
+++ pkg/services/fts/sqlite.go
@@ -0,0 +1,55 @@
+package fts
+
+import (
+	"context"
+
+	"github.com/grafana/grafana/pkg/infra/db"
+)
+
+type sqliteSearch struct {
+	db db.DB
+}
+
+func NewSQLiteSearch(db db.DB) (Search, error) {
+	m := &sqliteSearch{db: db}
+	return m, m.createTable()
+}
+
+func (m *sqliteSearch) createTable() error {
+	_, err := m.db.GetSqlxSession().Exec(context.Background(), `
+	CREATE VIRTUAL TABLE fts USING FTS4(text, kind, uid, org_id, weight);
+	`)
+	return err
+}
+
+func (m *sqliteSearch) Add(text, kind, uid string, orgID int64, weight int) error {
+	_, err := m.db.GetSqlxSession().Exec(context.Background(), `INSERT INTO fts(text, kind, uid, org_id, weight) VALUES(?, ?, ?, ?, ?)`, text, kind, uid, orgID, weight)
+	return err
+}
+
+func (m *sqliteSearch) Delete(kind, uid string, orgID int64) error {
+	_, err := m.db.GetSqlxSession().Exec(context.Background(), `DELETE FROM fts WHERE kind=? AND uid=? AND org_id=?`, kind, uid, orgID)
+	return err
+}
+
+func (m *sqliteSearch) Search(query string) ([]Result, error) {
+	rows, err := m.db.GetSqlxSession().Query(context.Background(), `
+	SELECT text, kind, uid, org_id, weight FROM fts WHERE text MATCH ?
+	`, query)
+	if err != nil {
+		return nil, err
+	}
+	results := []Result{}
+	for rows.Next() {
+		r := Result{}
+		f := 0.0
+		if err := rows.Scan(&r.Text, &r.Kind, &r.UID, &r.OrgID, &f); err != nil {
+			return nil, err
+		}
+		r.Weight = int(f * 100)
+		results = append(results, r)
+	}
+	return results, nil
+}
+
+func (m *sqliteSearch) Close() error { return nil }
---
+++ pkg/services/fts/sqlite_test.go
@@ -0,0 +1,49 @@
+package fts
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana/pkg/infra/db"
+)
+
+func TestSQLiteSimpleSearch(t *testing.T) {
+	db := db.InitTestDB(t)
+	search, err := NewSQLiteSearch(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, b := range books {
+		if err := search.Add(b, "book", b, 0, 1); err != nil {
+			t.Fatal(err)
+		}
+	}
+	if err := search.Delete("book", "Atlas Shrugged by Ayn Rand (1957)", 0); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, test := range []struct {
+		Query   string
+		Matches []string
+	}{
+		{"day", []string{"One Day in the Life of Ivan Denisovich by Alexander Solzhenitsyn (1962)"}},
+		{"sea", []string{"Wide Sargasso Sea by Jean Rhys (1966)", "The Sea, The Sea by Iris Murdoch (1978)"}},
+		{"count*", []string{"Another Country by James Baldwin (1962)", "The Count of Monte Cristo by Alexandre Dumas (1844)"}},
+		{"1938", []string{"The Death of the Heart by Elizabeth Bowen (1938)", "The Code of the Woosters by P. G. Wodehouse (1938)", "Scoop by Evelyn Waugh (1938)"}},
+		{"atlas", nil},
+	} {
+		res, err := search.Search(test.Query)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(test.Matches) != len(res) {
+			t.Fatal(res, test.Matches)
+		}
+		for i, m := range test.Matches {
+			t.Log(res[i])
+			if res[i].UID != m {
+				t.Fatal(res, test.Matches)
+			}
+		}
+	}
+}
