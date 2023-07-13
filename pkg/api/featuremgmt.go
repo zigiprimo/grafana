628f35a25cbcc7ec0bf38d2ecb4d9f3e322aa370
Joao Calisto <joao.santana.calisto@gmail.com>
2023-07-13 16:03:34 +0100

    Set the feature flag enabled field

--- pkg/api/featuremgmt.go
+++ pkg/api/featuremgmt.go
@@ -5,7 +5,16 @@ import (
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
 )
 
-func (hs *HTTPServer) GetFeatureToggles(c *contextmodel.ReqContext) response.Response {
+func (hs *HTTPServer) GetFeatureToggles(ctx *contextmodel.ReqContext) response.Response {
 	allFlags := hs.Features.GetFlags()
+	enabledFlags := hs.Features.GetEnabled(ctx.Req.Context())
+
+	for i, flag := range allFlags {
+		allFlags[i].Enabled = enabledFlags[flag.Name]
+	}
+
+	// TODO filter out hidden configs
+	// TODO set readonly field from configs
+
 	return response.JSON(200, allFlags)
 }
