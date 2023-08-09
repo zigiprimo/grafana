6b16ebbf7365af0af14b04dc7d1fa3b810f8f4e6
Roxana Turc <anamaria-roxana.turc@grafana.com>
2023-08-09 14:29:12 +0100

    Hackathon: Updated the look of the Spinner

---
+++ packages/grafana-ui/src/components/Spinner2/Spinner2.internal.story.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+
+import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
+
+import { Spinner2, Spinner2Props } from './Spinner2';
+
+const meta: Meta = {
+  title: 'Visualizations/Spinner2',
+  component: Spinner2,
+  decorators: [withCenteredStory],
+  parameters: {
+    args: {
+      size: 16,
+    },
+  },
+};
+
+export const Basic: StoryFn = (args: Spinner2Props) => {
+  return (
+    <div>
+      <Spinner2 size={args.size} color={args.color} />
+    </div>
+  );
+};
+
+Basic.args = {
+  size: 16,
+};
+
+export const Large: StoryFn = (args: Spinner2Props) => {
+  return (
+    <div>
+      <Spinner2 size={args.size} />
+    </div>
+  );
+};
+
+Large.args = {
+  size: 48,
+};
+
+export const Medium: StoryFn = (args: Spinner2Props) => {
+  return (
+    <div>
+      <Spinner2 size={args.size} />
+    </div>
+  );
+};
+
+Medium.args = {
+  size: 24,
+};
+
+export default meta;
---
+++ packages/grafana-ui/src/components/Spinner2/Spinner2.tsx
@@ -0,0 +1,129 @@
+import { css, keyframes } from '@emotion/css';
+import React, { useEffect, useState } from 'react';
+
+import { GrafanaTheme2 } from '@grafana/data';
+
+import { useTheme2 } from '../../themes';
+
+export interface Spinner2Props {
+  size?: number | string;
+  inline?: boolean;
+  color?: GrafanaTheme2 | string;
+}
+
+export const Spinner2 = ({ size = 16, inline = false }: Spinner2Props) => {
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (loading && count < 100) {
+      const timer = setTimeout(() => {
+        setCount(count + 1);
+      }, 300);
+      return () => clearTimeout(timer);
+    } else {
+      setLoading(false);
+    }
+
+    return () => {};
+  }, [loading, count]);
+
+  const theme = useTheme2();
+  const style = getStyles(theme, size);
+  return (
+    <div className={style.container}>
+      <div className={style.first}>
+        <div className={style.second}>
+          <div className={style.third}>
+            <div className={style.third}></div>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.counter}>{loading ? size === 16 ? '' : <p>{count}</p> : ''}</div>
+    </div>
+  );
+};
+
+const spinnerAnimation = keyframes`
+ 0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+// const counterAnimation = keyframes`
+//  from {
+//     --num: 1;
+//   }
+//   to {
+//     --num: 100;
+//   }
+//   `;
+
+const getStyles = (theme: GrafanaTheme2, size: number | string) => {
+  return {
+    container: css({
+      position: 'relative',
+      width: `${size}px`,
+      height: `${size}px`,
+      margin: '0 auto',
+    }),
+    first: css({
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+      borderRight: `1px solid ${theme.colors.text.disabled}`,
+      borderBottom: `1px solid ${theme.colors.text.disabled}`,
+      borderRadius: theme.shape.radius.circle,
+      position: 'relative',
+      margin: '0 auto',
+      animation: `${spinnerAnimation} 3s infinite linear`,
+    }),
+    second: css({
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '85%',
+      height: '85%',
+      borderRight: `1px solid ${theme.colors.text.disabled}`,
+      borderBottom: `1px solid ${theme.colors.text.disabled}`,
+      borderRadius: theme.shape.radius.circle,
+      position: 'absolute',
+      top: '3px',
+      left: ' 3px',
+      animation: `${spinnerAnimation} 3s infinite linear`,
+    }),
+    third: css({
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '75%',
+      height: '75%',
+      borderRight: `1px solid ${theme.colors.text.disabled}`,
+      borderBottom: `1px solid ${theme.colors.text.disabled}`,
+      borderRadius: theme.shape.radius.circle,
+      position: 'absolute',
+      top: '3px',
+      left: '3px',
+      animation: `${spinnerAnimation} 3s infinite linear`,
+    }),
+    counter: css({
+      position: 'absolute',
+      top: '70%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      /* animation: `${counterAnimation} 5s ease-in-out infinite`, */
+      /* counterReset: 'num var(--num)', */
+      font: '150 10px system-ui',
+      padding: '2rem',
+      /* '&:after': {
+        content: 'counter(num)',
+      }, */
+    }),
+  };
+};
