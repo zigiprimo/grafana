3dac7b5c13c686b0531b19051b36afd67c879e65
Piotr Jamróz <pm.jamroz@gmail.com>
2023-08-10 09:52:20 +0200

    Analyse dependencies

--- docs/dev/explore/analyse-dependencies.sh
+++ docs/dev/explore/analyse-dependencies.sh
@@ -8,6 +8,8 @@ ts_dependency_graph --hotspots --agg --graph_folder --max_depth=2 --filter="feat
 
 ts_dependency_graph --hotspots --agg --graph_folder --max_depth=3 --filter="features/explore,NOT features/explore/state/main" --start public/app/features/explore/state/main.ts | dot -Tpng -odocs/dev/explore/explore-state-3.png -v -Kfdp
 
+ts_dependency_graph --hotspots --max_depth=3 --filter="features/explore,NOT features/explore/state/main" --start public/app/features/explore/state/main.ts | dot -Tpng -odocs/dev/explore/explore-state-3-details.png -v
+
 ts_dependency_graph --hotspots --agg --graph_folder --max_depth=1 --filter="features/explore,NOT features/explore/ExplorePage.tsx" --start public/app/features/explore/ExplorePage.tsx | dot -Tpng -odocs/dev/explore/explore-ui-1.png -v -Kfdp
 
 ts_dependency_graph --hotspots --agg --graph_folder --max_depth=2 --filter="features/explore,NOT features/explore/ExplorePage.tsx" --start public/app/features/explore/ExplorePage.tsx | dot -Tpng -odocs/dev/explore/explore-ui-2.png -v -Kfdp
