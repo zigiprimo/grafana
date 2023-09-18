297e8387fc95ec1f94cadfc718a98ed471dd3d23
Gilles De Mey <gilles.de.mey@gmail.com>
2023-09-18 15:23:26 +0200

    implement a11y

--- public/app/features/alerting/unified/components/rule-list/RuleListView.v2.tsx
+++ public/app/features/alerting/unified/components/rule-list/RuleListView.v2.tsx
@@ -1,11 +1,12 @@
 import { css } from '@emotion/css';
-import React, { ReactNode } from 'react';
+import React, { PropsWithChildren, ReactNode } from 'react';
+import { useToggle } from 'react-use';
 
 import { GrafanaTheme2, IconName } from '@grafana/data';
 import { Stack } from '@grafana/experimental';
 import { Badge, Button, Dropdown, Icon, Link, Menu, Text, Tooltip, useStyles2 } from '@grafana/ui';
 
-import { AlertLabels } from '../AlertLabels';
+// import { AlertLabels } from '../AlertLabels';
 import { MetaText } from '../MetaText';
 import { Spacer } from '../Spacer';
 import { Strong } from '../Strong';
@@ -14,65 +15,71 @@ const RuleList = () => {
   const styles = useStyles2(getStyles);
 
   return (
-    <Stack direction="column">
-      <Namespace name="Demonstrations" />
-      <div className={styles.EvaluationGroupWrapper}>
-        <Stack direction="column" gap={0}>
-          <EvaluationGroupHeader expanded name={'default'} interval={'5 minutes'} />
-          <div className={styles.AlertRulesWrapper}>
-            <AlertRuleListItem
-              state="normal"
-              name={'CPU Usage'}
-              summary="The CPU usage is too high – investigate immediately!"
-            />
-            <AlertRuleListItem state="pending" name={'Memory Usage'} summary="Memory Usage too high" />
-            <AlertRuleListItem state="firing" name={'Network Usage'} summary="network congested" />
-          </div>
-        </Stack>
-        <Stack direction="column" gap={0}>
-          <EvaluationGroupHeader expanded name={'system metrics'} interval={'1 minute'} />
-          <div className={styles.AlertRulesWrapper}>
-            <AlertRuleListItem name={'email'} summary="gilles.demey@grafana.com" />
-          </div>
-        </Stack>
-      </div>
+    <ul className={styles.rulesTree} role="tree">
+      <Namespace name="Demonstrations">
+        <EvaluationGroup name={'default'} interval={'5 minutes'}>
+          <AlertRuleListItem
+            state="normal"
+            name={'CPU Usage'}
+            summary="The CPU usage is too high – investigate immediately!"
+          />
+          <AlertRuleListItem state="pending" name={'Memory Usage'} summary="Memory Usage too high" />
+          <AlertRuleListItem state="firing" name={'Network Usage'} summary="network congested" />
+        </EvaluationGroup>
 
-      <Namespace name="Network" />
-      <div className={styles.EvaluationGroupWrapper}>
-        <Stack direction="column" gap={0}>
-          <EvaluationGroupHeader expanded name={'UniFi Router'} provenance={'file'} interval={'2 minutes'} />
-          <div className={styles.AlertRulesWrapper}>
-            <Stack direction="column" gap={0}>
-              <AlertRuleListItem name={'CPU Usage'} summary="doing way too much work" isProvisioned={true} />
-              <AlertRuleListItem name={'Memory Usage'} isProvisioned={true} />
-            </Stack>
-          </div>
-        </Stack>
-      </div>
+        <EvaluationGroup name={'system metrics'} interval={'1 minute'}>
+          <AlertRuleListItem name={'email'} summary="gilles.demey@grafana.com" />
+        </EvaluationGroup>
+      </Namespace>
 
-      <Namespace name="System Metrics" />
-      <div className={styles.EvaluationGroupWrapper}>
-        <Stack direction="column" gap={0}>
-          <EvaluationGroupHeader name={'eu-west-1'} interval={'2 minutes'} />
-        </Stack>
-        <Stack direction="column" gap={0}>
-          <EvaluationGroupHeader name={'us-east-0'} interval={'5 minutes'} />
-        </Stack>
-      </div>
-    </Stack>
+      <Namespace name="Network">
+        <EvaluationGroup name={'UniFi Router'} provenance={'file'} interval={'2 minutes'}>
+          <AlertRuleListItem name={'CPU Usage'} summary="doing way too much work" isProvisioned={true} />
+          <AlertRuleListItem name={'Memory Usage'} isProvisioned={true} />
+        </EvaluationGroup>
+      </Namespace>
+
+      <Namespace name="System Metrics">
+        <EvaluationGroup name={'eu-west-1'} interval={'2 minutes'} />
+        <EvaluationGroup name={'us-east-0'} interval={'5 minutes'} />
+      </Namespace>
+    </ul>
   );
 };
 
-interface EvaluationGroupHeaderProps {
+interface EvaluationGroupProps extends PropsWithChildren {
   name: string;
-  expanded?: boolean;
   interval?: string;
   provenance?: string;
   description?: ReactNode;
 }
 
+// TODO toggling the namespace makes it forget what groups were toggled, hoist state?
+const EvaluationGroup = ({ children, name, provenance, interval }: EvaluationGroupProps) => {
+  const styles = useStyles2(getStyles);
+  const [isOpen, toggle] = useToggle(false);
+
+  return (
+    <div className={styles.groupWrapper} role="treeitem" aria-expanded={isOpen} aria-selected="false">
+      <EvaluationGroupHeader
+        onToggle={toggle}
+        provenance={provenance}
+        expanded={isOpen}
+        name={name}
+        interval={interval}
+      />
+      {isOpen && <div role="group">{children}</div>}
+    </div>
+  );
+};
+
+interface EvaluationGroupHeaderProps extends EvaluationGroupProps {
+  expanded?: boolean;
+  onToggle: () => void;
+}
+
 const EvaluationGroupHeader = (props: EvaluationGroupHeaderProps) => {
-  const { name, description, provenance, interval, expanded = false } = props;
+  const { name, description, provenance, interval, expanded = false, onToggle } = props;
 
   const styles = useStyles2(getStyles);
   const isProvisioned = Boolean(provenance);
@@ -80,10 +87,12 @@ const EvaluationGroupHeader = (props: EvaluationGroupHeaderProps) => {
   return (
     <div className={styles.headerWrapper}>
       <Stack direction="row" alignItems="center" gap={1}>
-        <Stack alignItems="center" gap={1}>
-          <Icon name={expanded ? 'angle-down' : 'angle-up'} />
-          <Text variant="body">{name}</Text>
-        </Stack>
+        <button className={styles.hiddenButton} type="button" onClick={onToggle}>
+          <Stack alignItems="center" gap={1}>
+            <Icon name={expanded ? 'angle-down' : 'angle-up'} />
+            <Text variant="body">{name}</Text>
+          </Stack>
+        </button>
         {isProvisioned && <Badge color="purple" text="Provisioned" />}
         {description && <MetaText>{description}</MetaText>}
         <Spacer />
@@ -152,14 +161,14 @@ const AlertRuleListItem = (props: AlertRuleListItemProps) => {
   };
 
   return (
-    <div className={styles.alertListItemWrapper}>
+    <li className={styles.alertListItemContainer} role="treeitem" aria-selected="false">
       <Stack direction="row" alignItems={'start'} gap={1}>
         <Text color={state ? color[state] : 'secondary'}>
           <Icon name={state ? icons[state] : 'circle'} size="lg" />
         </Text>
         <Stack direction="row" alignItems={'center'} gap={1} flexGrow={1}>
-          <Stack direction="column" gap={1}>
-            <div className={styles.receiverDescriptionRow}>
+          <Stack direction="column" gap={0.5}>
+            <div>
               <Stack direction="column" gap={0}>
                 <Stack direction="row" alignItems="center" gap={1}>
                   <Link href="/alerting/grafana/-amptgZVk/view">
@@ -183,7 +192,7 @@ const AlertRuleListItem = (props: AlertRuleListItemProps) => {
                 )}
               </Stack>
             </div>
-            <div className={styles.metadataRow}>
+            <div>
               <Stack direction="row" gap={1}>
                 {error ? (
                   <>
@@ -250,39 +259,57 @@ const AlertRuleListItem = (props: AlertRuleListItemProps) => {
           </Dropdown>
         </Stack>
       </Stack>
-    </div>
+    </li>
   );
 };
 
-interface NamespaceProps {
+interface NamespaceProps extends PropsWithChildren {
   name: string;
 }
 
-const Namespace = ({ name }: NamespaceProps) => {
+// TODO hook up buttons
+const Namespace = ({ children, name }: NamespaceProps) => {
   const styles = useStyles2(getStyles);
+  const [isOpen] = useToggle(true);
 
   return (
-    <div className={styles.namespaceWrapper}>
-      <Stack alignItems={'center'}>
-        <Stack alignItems={'center'} gap={1}>
-          <Icon name="folder-open" />
-          <Link href="/dashboards/f/lG5pfeRVk/demonstrations">
-            <Text color="link">{name}</Text>
-          </Link>
+    <li className={styles.namespaceWrapper} role="treeitem" aria-expanded={isOpen} aria-selected="false">
+      <div className={styles.namespaceTitle}>
+        <Stack alignItems={'center'}>
+          <Stack alignItems={'center'} gap={1}>
+            <Icon name={isOpen ? 'folder-open' : 'folder'} />
+            <Link href="/dashboards/f/lG5pfeRVk/demonstrations">
+              <Text color="link">{name}</Text>
+            </Link>
+          </Stack>
+          <Spacer />
+          <Button variant="secondary" size="sm" icon="unlock" type="button" aria-label="edit permissions">
+            Edit permissions
+          </Button>
         </Stack>
-        <Spacer />
-        <Button variant="secondary" size="sm" icon="unlock" type="button" aria-label="edit permissions">
-          Edit permissions
-        </Button>
-      </Stack>
-    </div>
+      </div>
+      {children && isOpen && (
+        <ul role="group" className={styles.groupItemsWrapper}>
+          {children}
+        </ul>
+      )}
+    </li>
   );
 };
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  EvaluationGroupWrapper: css`
+  rulesTree: css`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing(1.5)};
+  `,
+  groupWrapper: css`
+    display: flex;
+    flex-direction: column;
+  `,
+  groupItemsWrapper: css`
     position: relative;
-    border-radius: ${theme.shape.borderRadius()};
+    border-radius: ${theme.shape.radius.default};
     border: solid 1px ${theme.colors.border.weak};
     border-bottom: none;
 
@@ -299,8 +326,9 @@ const getStyles = (theme: GrafanaTheme2) => ({
       margin-left: -${theme.spacing(2.5)};
     }
   `,
-  alertListItemWrapper: css`
+  alertListItemContainer: css`
     position: relative;
+    list-style: none;
     background: ${theme.colors.background.primary};
 
     border-bottom: solid 1px ${theme.colors.border.weak};
@@ -311,21 +339,29 @@ const getStyles = (theme: GrafanaTheme2) => ({
 
     background: ${theme.colors.background.secondary};
 
+    border: none;
     border-bottom: solid 1px ${theme.colors.border.weak};
-    border-top-left-radius: ${theme.shape.borderRadius()};
-    border-top-right-radius: ${theme.shape.borderRadius()};
+    border-top-left-radius: ${theme.shape.radius.default};
+    border-top-right-radius: ${theme.shape.radius.default};
   `,
   namespaceWrapper: css`
+    display: flex;
+    flex-direction: column;
+
+    gap: ${theme.spacing(1.5)};
+  `,
+  namespaceTitle: css`
     padding: ${theme.spacing(1)} ${theme.spacing(1.5)};
 
     background: ${theme.colors.background.secondary};
 
     border: solid 1px ${theme.colors.border.weak};
-    border-radius: ${theme.shape.borderRadius()};
+    border-radius: ${theme.shape.radius.default};
   `,
-  receiverDescriptionRow: css``,
-  metadataRow: css``,
-  AlertRulesWrapper: css``,
+  hiddenButton: css`
+    border: none;
+    background: transparent;
+  `,
 });
 
 export default RuleList;
