5d36b36f20964809035c39441f7c2e271706d4ee
Ivan Ortega <ivanortegaalba@gmail.com>
2023-10-06 12:52:21 +0200

    Deep freeze originalDashboard to avoid mutations by object reference

--- public/app/features/dashboard/state/DashboardModel.ts
+++ public/app/features/dashboard/state/DashboardModel.ts
@@ -24,6 +24,7 @@ import { GRID_CELL_HEIGHT, GRID_CELL_VMARGIN, GRID_COLUMN_COUNT, REPEAT_DIR_VERT
 import { contextSrv } from 'app/core/services/context_srv';
 import { sortedDeepCloneWithoutNulls } from 'app/core/utils/object';
 import { isAngularDatasourcePlugin } from 'app/features/plugins/angularDeprecation/utils';
+import { deepFreeze } from 'app/features/plugins/extensions/utils';
 import { variableAdapters } from 'app/features/variables/adapters';
 import { onTimeRangeUpdated } from 'app/features/variables/state/actions';
 import { GetVariables, getVariablesByKey } from 'app/features/variables/state/selectors';
@@ -172,7 +173,8 @@ export class DashboardModel implements TimeModel {
     this.links = data.links ?? [];
     this.gnetId = data.gnetId || null;
     this.panels = map(data.panels ?? [], (panelData: any) => new PanelModel(panelData));
-    this.originalDashboard = data;
+    // Deep clone and freeze original dashboard to avoid mutations by object reference
+    this.originalDashboard = deepFreeze(data);
     this.ensurePanelsHaveUniqueIds();
     this.formatDate = this.formatDate.bind(this);
 
