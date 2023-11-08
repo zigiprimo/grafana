64e69ee313e9877d71ed6acc02a002026ba77623
Matias Chomicki <matyax@gmail.com>
2023-11-08 14:13:54 +0100

    Infinite scrolling: use scrollElement to listen to scrolling events

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -813,6 +813,7 @@ class UnthemedLogs extends PureComponent<Props, State> {
               addResultsToCache={addResultsToCache}
               clearCache={clearCache}
               loadMoreLogs={loadMoreLogs}
+              scrollElement={this.props.scrollElement}
             />
           </div>
         </PanelChrome>
