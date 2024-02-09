467462859e493338608d68cfcc16a52ce81dfd6a
Bryan Huhta <bryan.huhta@grafana.com>
2024-02-09 16:39:08 -0700

    Test export

--- packages/grafana-flamegraph/src/index.ts
+++ packages/grafana-flamegraph/src/index.ts
@@ -1,3 +1,7 @@
 export { default as FlameGraph, type Props } from './FlameGraphContainer';
 export { checkFields, getMessageCheckFieldsResult } from './FlameGraph/dataTransform';
 export { data } from './FlameGraph/testData/dataNestedSet';
+
+// TODO(bryan) Trying to export this and use it somewhere in the plugin code.
+const TEST_CONSTANT = 'MY CONSTANT';
+export { TEST_CONSTANT };
