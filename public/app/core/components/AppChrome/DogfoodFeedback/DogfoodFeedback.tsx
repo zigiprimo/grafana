115e3669c1f7b56f6fd1608688a43ebb40195a15
Elliot Kirk <elliot.kirk@grafana.com>
2024-03-07 13:05:53 -0800

    adding dog :)

--- public/app/core/components/AppChrome/DogfoodFeedback/DogfoodFeedback.tsx
+++ public/app/core/components/AppChrome/DogfoodFeedback/DogfoodFeedback.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import { getBackendSrv } from '@grafana/runtime';
-import { Drawer, FileUpload, TagsInput, TextArea, ToolbarButton } from '@grafana/ui';
+import { Button, Drawer, FileUpload, TagsInput, TextArea, ToolbarButton } from '@grafana/ui';
+
 import useRudderStack from './useRudderstack';
 
 export interface Props {}
@@ -11,7 +12,16 @@ export const DogfoodFeedback = ({}: Props) => {
   const [tags, setTags] = useState<string[]>([]);
   const [comment, setComment] = useState<string>('');
   const [files, setFiles] = useState<File[]>([]);
+  const [showDog, setShowDog] = useState<boolean>(false);
   const { trackRudderStackEvent } = useRudderStack();
+
+  useEffect(() => {
+    if (showDog) {
+      setShowDog(false);
+    }
+
+    return () => {};
+  }, [showDrawer])
 
   function createFeedback(event: any) {
     event.preventDefault();
@@ -19,7 +29,7 @@ export const DogfoodFeedback = ({}: Props) => {
     trackRudderStackEvent('feedr.feedback', { tags, comment });
 
     getBackendSrv()
-    .fetch({
+      .fetch({
         url: '/api/plugins/grafana-feedr-app/resources/feedback',
         method: 'POST',
         data: {
@@ -30,23 +40,24 @@ export const DogfoodFeedback = ({}: Props) => {
       .subscribe((response) => {
         console.log('response', response);
 
+        // @ts-ignore
         const issueId = response.data.issue_id;
 
-          console.log('file', files);
+        console.log('file', files);
 
-          const formData = new FormData();
-          formData.append('screenshot', files[0]);
+        const formData = new FormData();
+        formData.append('screenshot', files[0]);
 
-          const result = fetch(`/api/plugins/grafana-feedr-app/resources/feedback/upload?issueId=${issueId}`, {
-            method: "POST",
-            body: formData,
-          });
+        const result = fetch(`/api/plugins/grafana-feedr-app/resources/feedback/upload?issueId=${issueId}`, {
+          method: 'POST',
+          body: formData,
+        });
 
-          result.then((response) => {
-            setComment('');
-            setTags([]);
-            setFiles([]);
-          });
+        result.then((response) => {
+          setComment('');
+          setTags([]);
+          setFiles([]);
+        });
       });
 
     setShowDrawer(false);
@@ -64,35 +75,68 @@ export const DogfoodFeedback = ({}: Props) => {
         onClick={() => setShowDrawer(true)}
       />
       {showDrawer && (
-        <Drawer title="Dogfood feedback" size="sm" onClose={() => setShowDrawer(false)} expandable={false} >
-          <form onSubmit={createFeedback}>
-            <span style={{ display: 'block', margin: '5px', fontSize: '16px', lineHeight: '22px', fontWeight: '400' }}>
-              Hey Grafanista, what issue have you noticed on this page?
-            </span>
-            <TextArea placeholder="Describe the issue" rows={10} value={comment} onChange={e => setComment(e.target.value)}/>
-            <div style={{ margin: '50px 5px' }}>
-            <span style={{ display: 'block', margin: '5px', fontSize: '16px', lineHeight: '22px', fontWeight: '400' }}>
-              Add some tags to help us categorize the issue
-            </span>
-            <TagsInput tags={tags} onChange={setTags} />
-            </div>
-            <div style={{ margin: '5px' }}>
+        <Drawer title="Dogfood feedback" size="sm" onClose={() => setShowDrawer(false)} expandable={false}>
+          {showDog ? (
+            <div style={{ display: 'flex', flexFlow: 'column' }}>
               <span
-                style={{ marginBottom: '5px', display: 'block', fontSize: '16px', lineHeight: '22px', fontWeight: '400' }}
+                style={{ display: 'block', margin: '5px', fontSize: '16px', lineHeight: '22px', fontWeight: '400', textAlign: 'center' }}
               >
-                Do you want to add a screenshot?
+                Thank you for your feedback! Your issue has been reported here (or somewhere like that?)
               </span>
-              <FileUpload showFileName={true} onFileUpload={({ currentTarget }) => {
-                const files = Array.from(currentTarget.files || []) as File[];
-                setFiles(files);
-              }}/>
+              <img src={'public/img/dogs/dog1.gif'} alt="dog" width="240px" height="240px" style={{ margin: 'auto' }} />
             </div>
+          ) : (
+            <form onSubmit={createFeedback}>
+              <span
+                style={{ display: 'block', margin: '5px', fontSize: '16px', lineHeight: '22px', fontWeight: '400' }}
+              >
+                Hey Grafanista, what issue have you noticed on this page?
+              </span>
+              <TextArea
+                placeholder="Describe the issue"
+                rows={10}
+                value={comment}
+                // @ts-ignore
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <div style={{ margin: '50px 5px' }}>
+                <span
+                  style={{ display: 'block', margin: '5px', fontSize: '16px', lineHeight: '22px', fontWeight: '400' }}
+                >
+                  Add some tags to help us categorize the issue
+                </span>
+                <TagsInput tags={tags} onChange={setTags} />
+              </div>
+              <div style={{ margin: '5px' }}>
+                <span
+                  style={{
+                    marginBottom: '5px',
+                    display: 'block',
+                    fontSize: '16px',
+                    lineHeight: '22px',
+                    fontWeight: '400',
+                  }}
+                >
+                  Do you want to add a screenshot?
+                </span>
+                <FileUpload
+                  showFileName={true}
+                  onFileUpload={({ currentTarget }) => {
+                    const files = Array.from(currentTarget.files || []) as File[];
+                    setFiles(files);
+                  }}
+                />
+              </div>
 
-
-            <div style={{ margin: '5px' }}>
-              <button type="submit">Submit</button>
-            </div>
-          </form>
+              <div style={{ display: 'flex' }}>
+                <div style={{ margin: '250px auto' }}>
+                  <Button type="submit" onClick={() => setShowDog(true)} style={{margin: 'auto 5px'}}>
+                    Submit Feedback
+                  </Button>
+                </div>
+              </div>
+            </form>
+          )}
         </Drawer>
       )}
     </>
